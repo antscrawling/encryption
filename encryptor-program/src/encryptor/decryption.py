0d3f83be385d4b6d9412f8f6d57e25695fb2ccbb
Jose Ibay <ourcatisfat@gmail.com>
2025-01-11 15:43:58 +0800

    Implement missing functions in encryptor program
    updated version from github

--- encryptor-program/src/encryptor/decryption.py
+++ encryptor-program/src/encryptor/decryption.py
@@ -1,7 +1,16 @@
+from cryptography.fernet import Fernet
+import base64
+from database.db_setup import setup_database, EncryptedString
+
 def decrypt_string(encrypted_string, decryption_code):
-    # Implement the decryption logic here
-    pass
+    cipher = Fernet(base64.urlsafe_b64decode(decryption_code))
+    decrypted_string = cipher.decrypt(encrypted_string.encode()).decode()
+    return decrypted_string
 
 def get_decryption_code(alias):
-    # Retrieve the decryption code associated with the given alias
-    pass
+    session = setup_database()
+    encrypted_string_record = session.query(EncryptedString).filter_by(alias=alias).first()
+    session.close()
+    if encrypted_string_record:
+        return encrypted_string_record.decryption_code
+    return None
--- encryptor-program/src/user/user_management.py
+++ encryptor-program/src/user/user_management.py
@@ -1,3 +1,8 @@
+from sqlalchemy.orm import sessionmaker
+from database.db_setup import create_engine, User, EncryptedString
+from utils.clipboard import copy_to_clipboard
+from utils.logger import log_activity
+
 def create_user(username, password, first_name, last_name, user_type, secret_question, secret_answer, email, phone):
     # Logic to create a new user profile in the database
     
@@ -6,35 +11,124 @@ def create_user(username, password, first_name, last_name, user_type, secret_que
     
     
 def delete_user(username):
-    # Logic to delete the user profile from the database
-    pass
+    engine = create_engine("sqlite:///encryptor.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    user = session.query(User).filter_by(username=username).first()
+    if user:
+        session.delete(user)
+        session.commit()
+        log_activity(user=username, activity=f"Deleted user profile with username: {username}")
+        session.close()
+        return True
+    session.close()
+    return False
 
 def reset_password(username, secret_question, secret_answer, new_password):
-    # Logic to reset the user's password if the secret question and answer are correct
-    pass
+    engine = create_engine("sqlite:///encryptor.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    user = session.query(User).filter_by(username=username, secret_question=secret_question, secret_answer=secret_answer).first()
+    if user:
+        user.password = new_password
+        session.commit()
+        log_activity(user=username, activity=f"Reset password for user: {username}")
+        session.close()
+        return True
+    session.close()
+    return False
 
 def deactivate_user(username):
-    # Logic to deactivate the user's profile
-    pass
+    engine = create_engine("sqlite:///encryptor.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    user = session.query(User).filter_by(username=username).first()
+    if user:
+        user.active_status = False
+        session.commit()
+        log_activity(user=username, activity=f"Deactivated user profile with username: {username}")
+        session.close()
+        return True
+    session.close()
+    return False
 
 def view_user_data(username):
-    # Logic to retrieve and return the user's data
-    pass
+    engine = create_engine("sqlite:///encryptor.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    user = session.query(User).filter_by(username=username).first()
+    session.close()
+    if user:
+        return {
+            "username": user.username,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email,
+            "phone": user.phone,
+            "active_status": user.active_status,
+            "alias": user.alias
+        }
+    return None
 
 def list_user_aliases(username):
-    # Logic to list all aliases associated with the user's encrypted strings
-    pass
+    engine = create_engine("sqlite:///encryptor.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    user = session.query(User).filter_by(username=username).first()
+    session.close()
+    if user and user.alias:
+        return user.alias.split(',')
+    return []
 
 def copy_encrypted_string_and_key(alias):
-    # Logic to copy the encrypted string and decryption key to clipboard
-    pass
+    engine = create_engine("sqlite:///encryptor.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    encrypted_string_record = session.query(EncryptedString).filter_by(alias=alias).first()
+    session.close()
+    if encrypted_string_record:
+        copy_to_clipboard(f"Encrypted String: {encrypted_string_record.encrypted_string}, Decryption Key: {encrypted_string_record.decryption_code}")
+        log_activity(user=encrypted_string_record.username, activity=f"Copied encrypted string and key for alias: {alias}")
+        return True
+    return False
 
 def delete_encrypted_string(alias):
-    # Logic to delete the encrypted string associated with the alias
-    pass
+    engine = create_engine("sqlite:///encryptor.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    encrypted_string_record = session.query(EncryptedString).filter_by(alias=alias).first()
+    if encrypted_string_record:
+        session.delete(encrypted_string_record)
+        session.commit()
+        log_activity(user=encrypted_string_record.username, activity=f"Deleted encrypted string with alias: {alias}")
+        session.close()
+        return True
+    session.close()
+    return False
 
 def generate_user_report():
-    # Logic to generate a report of user information
-    pass
+    engine = create_engine("sqlite:///encryptor.db")
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-
+    users = session.query(User).all()
+    session.close()
+    report = []
+    for user in users:
+        report.append({
+            "username": user.username,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "email": user.email,
+            "phone": user.phone,
+            "active_status": user.active_status,
+            "alias": user.alias
+        })
+    return report
